03531d57fede404b48040f112daf221e52c7dc1a
junaidjaan1388 <112433148+junaidjaan1388@users.noreply.github.com>
2022-09-28 13:39:12 +0600

    Update DownloadOptions.php

--- src/DownloadOptions.php
+++ src/DownloadOptions.php
@@ -51,7 +51,7 @@ class DownloadOptions
     {
         return Utils::arrayFilterReset($this->getAllFormats(), function ($format) {
             /** @var $format StreamFormat */
-            return strpos($format->mimeType, 'audio') === 0;
+            return strpos($format->mimeType, 'video') === 0;
         });
     }
 
@@ -59,7 +59,7 @@ class DownloadOptions
     {
         return Utils::arrayFilterReset($this->getAllFormats(), function ($format) {
             /** @var $format StreamFormat */
-            return strpos($format->mimeType, 'video') === 0 && !empty($format->audioQuality);
+            return strpos($format->mimeType, 'video') === 0 && !empty($format->videoQuality);
         });
     }
 
